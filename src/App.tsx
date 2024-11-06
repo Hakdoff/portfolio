4b40009dfeca093b1e390329f58f5c9da789b53b
ErikaFuerte <erika@bpoc.co.jp>
2024-11-06 14:23:17 +0800

    fix deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import Portfolio from './portfolio/portfolio'
+import { BrowserRouter } from 'react-router-dom';
+import Portfolio from './portfolio/portfolio';
 
 function App() {
-
   return (
-    <>
-      <Portfolio></Portfolio>
-    </>
-  )
+    <BrowserRouter basename="/portfolio_react">
+      <Portfolio />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
